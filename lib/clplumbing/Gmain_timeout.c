93f659b740b5c6f9f8948d03016d56d77711137c
Alan Robertson <alanr@unix.sh>
2002-05-22 04:07:57 +0000

    Added a copyright and license statement.
    CVS patchset: 1075
    CVS date: 2002/05/22 04:07:57

--- lib/clplumbing/Gmain_timeout.c
+++ lib/clplumbing/Gmain_timeout.c
@@ -1,15 +1,35 @@
-#include <glib.h>
-#include <clplumbing/longclock.h>
-#include <clplumbing/Gmain_timeout.h>
 /*
- * These functions must work correctly even if someone resets the 
+ * Glib mainloop timeout handling code.
+ *
+ * These functions work correctly even if someone resets the 
  * time-of-day clock.  The g_main_timeout_add() function does not have
  * this property, since it relies on gettimeofday().
  *
  * Our functions have the same semantics - except they always work ;-)
  *
  * This is because we use longclock_t for our time values.
+ *
+ * Copyright (c) 2002 Alan Robertson <alanr@unix.sh>
+ *
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
+#include <glib.h>
+#include <clplumbing/longclock.h>
+#include <clplumbing/Gmain_timeout.h>
 
 static struct GTimeoutSource*
 Gmain_TimeoutSource_new(guint interval, GSourceFunc f);
