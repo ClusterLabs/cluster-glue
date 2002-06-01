e7a1a48e6fcd427770c4ce7d789a3f2d34332e47
alan <unknown>
2002-06-01 02:37:24 +0000

    Added the .c files for Xiaoxiang Liu's IPC API
    CVS patchset: 1081
    CVS date: 2002/06/01 02:37:24

---
+++ lib/clplumbing/ocf_ipc.c
@@ -0,0 +1,76 @@
+/*!
+ \file
+ \brief IPC abstraction implementation.
+ \author Xiaoxiang Liu <xiliu@ncsa.uiuc.edu>
+*/
+/*
+ * Copyright (c) 2002 Xiaoxiang Liu <xiliu@ncsa.uiuc.edu>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+#include <clplumbing/ipc.h>
+
+struct OCF_IPC_WAIT_CONNECTION * socket_wait_conn_new(GHashTable* ch_attrs);
+struct OCF_IPC_CHANNEL * socket_channel_new(GHashTable* ch_attrs);
+
+struct OCF_IPC_WAIT_CONNECTION * 
+ipc_wait_conn_constructor(char * ch_type, GHashTable* ch_attrs) {
+  if (strcmp(ch_type, "domain_socket") == 0) {
+    return socket_wait_conn_new(ch_attrs);
+  }
+  return NULL;
+}
+
+struct OCF_IPC_CHANNEL * 
+ipc_channel_constructor(char * ch_type, GHashTable* ch_attrs) {
+  if (strcmp(ch_type, "domain_socket") == 0) {    
+    return socket_channel_new(ch_attrs);
+  }
+    return NULL;
+}
+
+
+struct OCF_IPC_AUTH * 
+ipc_set_auth(uid_t * a_uid, gid_t * a_gid, int num_uid, int num_gid) {
+  struct OCF_IPC_AUTH *temp_auth;
+  int i, v;
+
+  v = 1;
+  temp_auth = g_new(struct OCF_IPC_AUTH, 1);
+  temp_auth->uid = g_hash_table_new(g_int_hash, g_int_equal);
+  temp_auth->gid = g_hash_table_new(g_int_hash, g_int_equal);
+
+  if (num_uid > 0) {
+    temp_auth->check_uid = TRUE;
+    for(i=0;i<num_uid;i++)
+      g_hash_table_insert(temp_auth->uid, &a_uid[i], &v);
+  }else{
+    temp_auth->uid = FALSE;
+  }
+
+  if (num_gid > 0) {
+    temp_auth->check_gid = TRUE;
+    for(i=0;i<num_gid;i++)
+      g_hash_table_insert(temp_auth->gid, &a_gid[i], &v);
+  }else{
+    temp_auth->gid = FALSE;
+  }
+
+  return temp_auth;
+}
+
+    
+  
