45636f7b6fc8293491c24d16d0b1c65d4d26947f
Alan Robertson <alanr@unix.sh>
2002-08-16 01:33:28 +0000

    Changed some parameters to const as per a patch from Ram.
    CVS patchset: 1239
    CVS date: 2002/08/16 01:33:28

--- include/clplumbing/base64.h
+++ include/clplumbing/base64.h
@@ -13,17 +13,22 @@
 #define	B64inunit	3
 #define	B64outunit	4
 
-/* How many bytes will a binary object taken when converted to base64? */
+/* How long will the base64 string be for a particular binary object size? */
+/* This is like strlen() and doesn't include the '\0' byte at the end */
 #define	B64_stringlen(bytes)	\
 	((((bytes)+(B64inunit-1))/B64inunit)*B64outunit)
+
+/* How many bytes to you need to malloc to store a base64 string? */
+/* (includes space for the '\0' terminator byte) */
+#define	B64_stringspace(bytes)	(B64_stringlen(bytes)+1)
 
 /* How many bytes will a base64 string take up back in binary? */
 /* Note:  This may be as much as two 2 bytes more than strictly needed */
 #define	B64_maxbytelen(slen)	(((slen) / B64outunit)*B64inunit)
 
 /* Returns strlen() of base64 string returned in "output" */
-int binary_to_base64(void * data, int nbytes, char * output, int outlen);
+int binary_to_base64(const void * data, int nbytes, char * output, int outlen);
 
 /* Returns the size of the binary object we returned in "output" */
-int base64_to_binary(char * input, int inlen, void * output, int outlen);
+int base64_to_binary(const char * input, int inlen, void * output, int outlen);
 #endif
