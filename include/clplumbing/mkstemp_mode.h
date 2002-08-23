7a4a65c1fa06dcd7c2b547b34abca1da8f1f9cdf
alan <unknown>
2002-08-23 15:15:12 +0000

    Added the forgotten mkstemp_mode() header file.
    CVS patchset: 1271
    CVS date: 2002/08/23 15:15:12

---
+++ include/clplumbing/mkstemp_mode.h
@@ -0,0 +1,18 @@
+/*
+ * A slightly safer version of mkstemp(3)
+ *
+ * In this version, the file is initially created mode 0, (using umask) and
+ * then chmod-ed to the requested permissions after calling mkstemp(3).
+ * This guarantees that the file is not even momentarily open beyond the
+ * requested permissions.
+ *
+ * Return values:
+ *
+ * Like mkstemp, it returns the file descriptor of the open file, or -1
+ * on error.
+ *
+ * In addition to the errno values documented for mkstemp(3), this functio
+ * can also fail with any of the errno values documented for chmod(2).
+ *
+ */
+int mkstemp_mode(char* template, mode_t requested_filemode);
