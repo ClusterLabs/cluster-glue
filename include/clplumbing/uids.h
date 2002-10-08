0f4c93678cc6fd9e2d5a142913c826e80969d75d
Alan Robertson <alanr@unix.sh>
2002-10-08 03:40:37 +0000

    An attempt to fix Matt's problem which appears to be the result of dropping privileges incorrectly.
    CVS patchset: 1405
    CVS date: 2002/10/08 03:40:37

--- include/clplumbing/uids.h
+++ include/clplumbing/uids.h
@@ -2,7 +2,12 @@
 #	define CLPLUMBING_UIDS_H
 #include <sys/types.h>
 
+/* Tell us who you want to be - or zero for nobody */
 int drop_privs(uid_t uid, gid_t gid);
+
+/* Return to original privileged state */
 int return_to_orig_privs(void);
+
+/* Drop down to (probably nobody) privileges again */
 int return_to_dropped_privs(void);
 #endif
--- lib/clplumbing/uids.c
+++ lib/clplumbing/uids.c
@@ -6,11 +6,29 @@
 #include <clplumbing/uids.h>
 #define	NOBODY	"nobody"
 
+#if defined(HAVE_SETEUID) && defined(HAVE_SETEGID) &&	\
+		 defined(_POSIX_SAVED_IDS)
+#	define	CAN_DROP_PRIVS	1
+
+#endif
+
+
+
+
+#ifndef CAN_DROP_PRIVS
+	int drop_privs(uid_t uid, gid_t gid)	{	return 0;	}
+	int return_to_orig_privs(void)		{	return 0;	}
+	int return_to_dropped_privs(void)	{	return 0;	}
+#else
+
 static int	anysaveduid = 0;
 static uid_t	nobodyuid=-1;
 static gid_t	nobodygid=-1;
 static uid_t	poweruid=-1;
 static gid_t	powergid=-1;
+
+/*	WARNING: uids are unsigned! */
+#define	WANT_NOBODY(uid)	((uid) == 0)
 
 int	/* Become nobody - and remember our original privileges */
 drop_privs(uid_t uid, gid_t gid)
@@ -23,7 +41,7 @@ drop_privs(uid_t uid, gid_t gid)
 		powergid=curgid;
 	}
 
-	if (uid <= 0) {
+	if (WANT_NOBODY(uid)) {
 		struct passwd*	p;
 
 		p = getpwnam(NOBODY);
@@ -34,10 +52,10 @@ drop_privs(uid_t uid, gid_t gid)
 		uid = p->pw_uid;
 		gid = p->pw_gid;
 	}
-	if (setgid(gid) < 0) {
+	if (setegid(gid) < 0) {
 		return -1;
 	}
-	rc = setuid(uid);
+	rc = seteuid(uid);
 
 	if (rc >= 0) {
 		anysaveduid = 1;
@@ -46,7 +64,7 @@ drop_privs(uid_t uid, gid_t gid)
 	}else{
 		/* Attempt to recover original privileges */
 		int	err = errno;
-		setgid(curgid);
+		setegid(curgid);
 		errno = err;
 	}
 	return rc;
@@ -58,10 +76,10 @@ return_to_orig_privs()
 	if (!anysaveduid) {
 		return 0;
 	}
-	if (setuid(poweruid) < 0) {
+	if (seteuid(poweruid) < 0) {
 		return -1;
 	}
-	return setgid(powergid);
+	return setegid(powergid);
 }
 
 int	/* Return to "nobody" level of privs (if any) */
@@ -70,6 +88,7 @@ return_to_dropped_privs(void)
 	if (!anysaveduid) {
 		return 0;
 	}
-	setgid(nobodygid);
-	return setuid(nobodyuid);
+	setegid(nobodygid);
+	return seteuid(nobodyuid);
 }
+#endif
