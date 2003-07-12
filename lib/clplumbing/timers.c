2e57db48ecc7da55998b5cabd12a1381d118fd8d
Alan Robertson <alanr@unix.sh>
2003-07-12 13:13:30 +0000

    Added the mssleep() function.
    CVS patchset: 2101
    CVS date: 2003/07/12 13:13:30

--- lib/clplumbing/timers.c
+++ lib/clplumbing/timers.c
@@ -1,10 +1,13 @@
 #include <portability.h>
 #include <sys/time.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <clplumbing/timers.h>
 #include <clplumbing/cl_log.h>
 #include <clplumbing/cl_signal.h>
+#include <clplumbing/longclock.h>
 
 int
 setmsrepeattimer(long	ms)
@@ -44,4 +47,53 @@ cancelmstimer(void)
 	,	{0L, 0L}	/* Timer Value */
 	};
 	return setitimer(ITIMER_REAL, &nexttime, NULL);
+}
+
+
+static int alarmpopped = 0;
+
+static void
+st_timer_handler(int nsig)
+{
+	++alarmpopped;
+}
+
+/*
+ * Pretty simple:
+ * 1) Set up SIGALRM signal handler
+ * 2) set alarmpopped to FALSE;
+ * 2) Record current time
+ * 3) Call setmsalarm(ms)
+ * 4) Call pause(2)
+ * 5) Call cancelmstimer()
+ * 6) Reset signal handler
+ * 7) See if SIGALRM happened
+ *    if so:  return zero
+ *    if not: get current time, and compute milliseconds left 'til signal
+ *	should arrive, and return that...
+ */
+long
+mssleep(long ms)
+{
+	struct sigaction	saveaction;
+	longclock_t		start;
+	longclock_t		finish;
+	unsigned long		elapsedms;
+
+	memset(&saveaction, 0, sizeof(saveaction));
+
+	cl_signal_set_simple_handler(SIGALRM, st_timer_handler, &saveaction);
+	alarmpopped = 0;
+	start = time_longclock();
+	setmsalarm(ms);
+	pause();
+	cancelmstimer();
+	cl_signal_set_simple_handler(SIGALRM, saveaction.sa_handler, &saveaction);
+	if (alarmpopped) {
+		return 0;
+	}
+	
+	finish = time_longclock();
+	elapsedms = longclockto_ms(sub_longclock(finish, start));
+	return ms - elapsedms;
 }
