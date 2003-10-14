600afbb05095f1aa03e2a78ed5b91005ab78fe0d
alan <unknown>
2003-10-14 18:32:48 +0000

    Added Yixiong Zou's IPMIlan STONITH plugin to CVS
    CVS patchset: 2205
    CVS date: 2003/10/14 18:32:48

---
+++ lib/plugins/stonith/ipmilan.c
@@ -0,0 +1,668 @@
+/*
+ * Stonith module for ipmi lan Stonith device
+ *
+ * Copyright (c) 2003 Intel Corp. 
+ *	Yixiong Zou <yixiong.zou@intel.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+/*
+ * See RADEME.ipmi for information regarding this plugin.
+ *
+ */
+
+#include <portability.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <syslog.h>
+#include <libintl.h>
+#include <sys/wait.h>
+
+#include <stonith/stonith.h>
+
+#define PIL_PLUGINTYPE          STONITH_TYPE
+#define PIL_PLUGINTYPE_S        STONITH_TYPE_S
+#define PIL_PLUGIN              ipmilan
+#define PIL_PLUGIN_S            "ipmilan"
+#define PIL_PLUGINLICENSE 	LICENSE_LGPL
+#define PIL_PLUGINLICENSEURL 	URL_LGPL
+#include <pils/plugin.h>
+
+#include <OpenIPMI/ipmi_types.h>
+#include <OpenIPMI/ipmi_auth.h>
+
+#include "ipmilan.h"
+
+/*
+ * ipmilanclose is called as part of unloading the ipmilan STONITH plugin.
+ * If there was any global data allocated, or file descriptors opened, etc.
+ * which is associated with the plugin, and not a single interface
+ * in particular, here's our chance to clean it up.
+ */
+
+static void
+ipmilanclosepi(PILPlugin*pi)
+{
+}
+
+
+/*
+ * ipmilancloseintf called as part of shutting down the ipmilan STONITH
+ * interface.  If there was any global data allocated, or file descriptors
+ * opened, etc.  which is associated with the ipmilan implementation,
+ * here's our chance to clean it up.
+ */
+static PIL_rc
+ipmilancloseintf(PILInterface* pi, void* pd)
+{
+	return PIL_OK;
+}
+
+static void *		ipmilan_new(void);
+static void		ipmilan_destroy(Stonith *);
+static int		ipmilan_set_config_file(Stonith *, const char * cfgname);
+static int		ipmilan_set_config_info(Stonith *, const char * info);
+static const char *	ipmilan_getinfo(Stonith * s, int InfoType);
+static int		ipmilan_status(Stonith * );
+static int		ipmilan_reset_req(Stonith * s, int request, const char * host);
+static char **		ipmilan_hostlist(Stonith  *);
+static void		ipmilan_free_hostlist(char **);
+
+static struct stonith_ops ipmilanOps ={
+	ipmilan_new,		/* Create new STONITH object	*/
+	ipmilan_destroy,		/* Destroy STONITH object	*/
+	ipmilan_set_config_file,	/* set configuration from file	*/
+	ipmilan_set_config_info,	/* Get configuration from file	*/
+	ipmilan_getinfo,		/* Return STONITH info string	*/
+	ipmilan_status,		/* Return STONITH device status	*/
+	ipmilan_reset_req,		/* Request a reset */
+	ipmilan_hostlist,		/* Return list of supported hosts */
+	ipmilan_free_hostlist	/* free above list */
+};
+
+PIL_PLUGIN_BOILERPLATE("1.0", Debug, ipmilanclosepi);
+static const PILPluginImports*  PluginImports;
+static PILPlugin*               OurPlugin;
+static PILInterface*		OurInterface;
+static StonithImports*		OurImports;
+static void*			interfprivate;
+
+#define LOG		PluginImports->log
+#define MALLOC		PluginImports->alloc
+#define FREE		PluginImports->mfree
+#define EXPECT_TOK	OurImports->ExpectToken
+#define STARTPROC	OurImports->StartProcess
+
+PIL_rc
+PIL_PLUGIN_INIT(PILPlugin*us, const PILPluginImports* imports);
+
+PIL_rc
+PIL_PLUGIN_INIT(PILPlugin*us, const PILPluginImports* imports)
+{
+	/* Force the compiler to do a little type checking */
+	(void)(PILPluginInitFun)PIL_PLUGIN_INIT;
+
+	PluginImports = imports;
+	OurPlugin = us;
+
+	/* Register ourself as a plugin */
+	imports->register_plugin(us, &OurPIExports);  
+
+	/*  Register our interface implementation */
+ 	return imports->register_interface(us, PIL_PLUGINTYPE_S
+	,	PIL_PLUGIN_S
+	,	&ipmilanOps
+	,	ipmilancloseintf		/*close */
+	,	&OurInterface
+	,	(void*)&OurImports
+	,	&interfprivate); 
+}
+
+#define	DEVICE	"ipmilan STONITH device"
+#define WHITESPACE	" \t\n\r\f"
+
+/*
+ *	ipmilan STONITH device.  
+ * 
+ * 	ipmilanHostInfo is a double linked list. Where the prev of the head always
+ *	points to the tail.  This is a little wierd.  But it saves me from looping
+ *	around to find the tail when destroying the list.
+ */
+
+struct ipmilanDevice {
+	const char *	ipmiid;
+	int		hostcount;
+	struct ipmilanHostInfo * 	hostlist;
+};
+
+static const char * ipmilanid = "ipmilanDevice-Stonith";
+static const char * NOTipmilanID = "Hey, dummy this has been destroyed (ipmilanDev)";
+
+#define	ISipmilanDEV(i)	(((i)!= NULL && (i)->pinfo != NULL)	\
+	&& ((struct ipmilanDevice *)(i->pinfo))->ipmiid == ipmilanid)
+
+
+#ifndef MALLOC
+#	define	MALLOC	malloc
+#endif
+#ifndef FREE
+#	define	FREE	free
+#endif
+#ifndef MALLOCT
+#	define     MALLOCT(t)      ((t *)(MALLOC(sizeof(t)))) 
+#endif
+
+#define N_(text)	(text)
+#define _(text)		dgettext(ST_TEXTDOMAIN, text)
+
+/*
+ * Check the status of the IPMI Lan STONITH device. 
+ * 
+ * NOTE: not sure what we should do here since each host is configured
+ * seperately.
+ *     
+ * Two options: 
+ *   1) always return S_OK. 
+ *   2) using IPMI ping to confirm the status for every host that's
+ *      configured. 
+ * 
+ * For now I choose the option 1 hoping that I can get by. Maybe we should
+ * change it to option 2 later. 
+ */
+
+static int
+ipmilan_status(Stonith  *s)
+{
+	struct ipmilanDevice * nd;
+	struct ipmilanHostInfo * node;
+	int ret;
+
+	if (!ISipmilanDEV(s)) {
+		syslog(LOG_ERR, "invalid argument to ipmilan_status");
+		return(S_OOPS);
+	}
+	ret = S_OK;
+
+	nd = (struct ipmilanDevice *)s->pinfo;
+	node = nd->hostlist;
+#if 0
+	do {
+		ret = send_ipmi_msg(node, ST_IPMI_STATUS);
+		if (ret) {
+			syslog(LOG_INFO, _("Host %s ipmilan status failure."), node->hostname);
+			ret = S_ACCESS;
+		} else {
+			syslog(LOG_INFO, _("Host %s ipmilan status OK."), node->hostname);
+		}
+		node = node->next;
+
+	} while (node);
+#endif
+	return ret;
+}
+
+/*
+ * This function returns the list of hosts that's configured. 
+ *
+ * The detailed configuration is disabled because the STONITH command can be
+ * run by anyone so there is a security risk if that to be exposed.
+ */
+
+static char *
+get_config_string(struct ipmilanDevice * nd, int index)
+{
+	struct ipmilanHostInfo * host;
+	int i, size;
+
+	char * buf, *p;
+
+	if (index >= nd->hostcount || index < 0) {
+		return (NULL);
+	}
+
+	host = nd->hostlist;
+	for (i = 0; i < index; i++) {
+		host = host->next;
+	}
+
+	size = strlen(host->hostname) + 1;
+
+#if 0
+	// 4 strings, 3 numbers, 6 blanks, 1 last trailing char.
+	size = strlen(host->hostname) + strlen(host->ipaddr) + 
+		16 + 16 + 8 + 1 + 1 + 6 + 1;
+#endif
+
+	buf = (char * )MALLOC(size);
+	if (!buf) {
+		return (NULL);
+	}
+
+	p = buf;
+	strncpy(p, host->hostname, strlen(host->hostname));
+	p += strlen(host->hostname);
+
+#if 0
+	strncpy(p, " ", 1);
+	p ++;
+	strncpy(p, host->ipaddr, strlen(host->ipaddr));
+	p += strlen(host->ipaddr); 
+	strncpy(p, " ", 1);
+	p ++;
+
+	strncpy(p, host->username, strlen(host->username));
+	p += strlen(host->username);
+	strncpy(p, " ", 1);
+	p ++;
+
+	strncpy(p, host->password, strlen(host->password));
+	p += strlen(host->password);
+	strncpy(p, " ", 1);
+	p ++;
+#endif
+
+	*p = '\0';
+
+	return buf;
+}
+
+
+/*
+ *	Return the list of hosts configured for this ipmilan device
+ *	
+ */
+
+static char **
+ipmilan_hostlist(Stonith  *s)
+{
+	int		numnames = 0;
+	char **		ret = NULL;
+	struct ipmilanDevice*	nd;
+	int		j;
+
+	if (!ISipmilanDEV(s)) {
+		syslog(LOG_ERR, "invalid argument to ipmilan_hostlist");
+		return(NULL);
+	}
+	nd = (struct ipmilanDevice*) s->pinfo;
+	if (nd->hostcount < 0) {
+		syslog(LOG_ERR
+		,	"unconfigured stonith object in ipmi_hostlist");
+		return(NULL);
+	}
+	numnames = nd->hostcount;
+
+	ret = (char **)MALLOC((numnames + 1)*sizeof(char*));
+	if (ret == NULL) {
+		syslog(LOG_ERR, "out of memory");
+		return (ret);
+	}
+
+	memset(ret, 0, (numnames + 1)*sizeof(char*));
+
+	for (j = 0; j < numnames; ++j) {
+		ret[j] = get_config_string(nd, j);
+		if (!ret[j]) {
+			ipmilan_free_hostlist(ret);
+			ret = NULL;
+			break;
+		}
+	}
+
+	return(ret);
+}
+
+static void
+ipmilan_free_hostlist (char ** hlist)
+{
+	char **	hl = hlist;
+	if (hl == NULL) {
+		return;
+	}
+	while (*hl) {
+		FREE(*hl);
+		*hl = NULL;
+		++hl;
+	}
+	FREE(hlist);
+	hlist = NULL;
+}
+
+
+/*
+ *	Parse the config information, and stash it away...
+ *
+ *	The buffer for each string is MAX_IPMI_STRING_LEN bytes long.
+ *      Right now it is set to 64. Hope this is enough.
+ *	
+ */
+
+#define MAX_IPMI_STRING_LEN 64
+
+static int
+ipmilan_parse_config_info(struct ipmilanDevice* nd, const char * info)
+{
+	static int port;
+
+	static char name[MAX_IPMI_STRING_LEN];
+	static char ip[MAX_IPMI_STRING_LEN]; 
+
+	static char auth[MAX_IPMI_STRING_LEN];
+	static char priv[MAX_IPMI_STRING_LEN];
+
+	static char user[MAX_IPMI_STRING_LEN];
+	static char pass[MAX_IPMI_STRING_LEN];
+
+	struct ipmilanHostInfo * hostinfo, * head, * tail;
+
+	port = 0;
+	hostinfo = NULL;
+	memset(name, 0, MAX_IPMI_STRING_LEN);
+	memset(ip, 0, MAX_IPMI_STRING_LEN);
+	memset(auth, 0, MAX_IPMI_STRING_LEN);
+	memset(priv, 0, MAX_IPMI_STRING_LEN);
+	memset(user, 0, MAX_IPMI_STRING_LEN);
+	memset(pass, 0, MAX_IPMI_STRING_LEN);
+
+	do {
+		if (sscanf(info, "%s %s %i %s %s %s %[^\r\n\t]", 
+			name, ip, &port, auth, priv, user, pass) == 7 && 
+			strlen(user) > 1 && strlen(pass) > 1) {
+
+			if ((hostinfo = (struct ipmilanHostInfo *) MALLOC(sizeof(struct ipmilanHostInfo))) == NULL) {
+				syslog(LOG_ERR, "out of memory");
+				return (S_OOPS);
+			}
+
+			hostinfo->portnumber = port;
+
+			if (strncmp(auth, "none", strlen(auth)) == 0) {
+				hostinfo->authtype = IPMI_AUTHTYPE_NONE;
+			} 
+			else if (strncmp(auth, "md2", strlen(auth)) == 0) {
+				hostinfo->authtype = IPMI_AUTHTYPE_MD2;
+			}
+			else if (strncmp(auth, "md5", strlen(auth)) == 0) {
+				hostinfo->authtype = IPMI_AUTHTYPE_MD5;
+			}
+			else if (strncmp(auth, "straight", strlen(auth)) == 0) {
+				hostinfo->authtype = IPMI_AUTHTYPE_STRAIGHT;
+			}
+			else {
+				break;
+			}
+
+			if (strncmp(priv, "admin", strlen(priv)) == 0) {
+				hostinfo->privilege = IPMI_PRIVILEGE_ADMIN;
+			}
+			else if (strncmp(priv, "operator", strlen(priv)) == 0) {
+				hostinfo->privilege = IPMI_PRIVILEGE_OPERATOR;
+			}
+			else {
+				break;
+			}
+
+			if ((hostinfo->hostname = (char *) MALLOC(strlen(name)+1)) == NULL) {
+				break;
+			}
+
+			if ((hostinfo->ipaddr = (char *) MALLOC(strlen(ip)+1)) == NULL) {
+				FREE(hostinfo->hostname);
+				break;
+			}
+
+			strncpy(hostinfo->hostname, name, strlen(name)+1);
+			strncpy(hostinfo->ipaddr, ip, strlen(ip)+1);
+
+			if (strncmp(user, "\"\"", 2)==0) {
+				memset(hostinfo->username, 0, sizeof(hostinfo->username));
+			} else {
+				strncpy(hostinfo->username, user, strlen(user)+1);
+			}
+
+			if (strncmp(pass, "\"\"", 2)==0) {
+				memset(hostinfo->password, 0, sizeof(hostinfo->password));
+			} else {
+				strncpy(hostinfo->password, pass, strlen(pass)+1);
+			}
+
+			hostinfo->next = NULL;
+
+			head = nd->hostlist;
+			// find the last one in the list
+			if (head) {
+				tail = head->prev;
+				tail->next = hostinfo;
+
+				hostinfo->prev = tail;
+				head->prev = hostinfo;
+
+			} else {
+				nd->hostlist = hostinfo;
+				hostinfo->prev = hostinfo;
+			}
+
+			// increment the host counter
+			nd->hostcount++;
+
+			return (S_OK);
+		} 
+		else {
+			break;
+		}
+
+	} while (0); // using this do loop here so we can have 'break' statement.
+
+	if (hostinfo) {
+		FREE(hostinfo);
+		hostinfo = NULL;
+	}
+	return (S_BADCONFIG);
+}
+
+/*
+ *	Reset the given host on this Stonith device.
+ */
+static int
+ipmilan_reset_req(Stonith * s, int request, const char * host)
+{
+	int rc = 0;
+	struct ipmilanDevice * nd;
+	struct ipmilanHostInfo * node;
+
+	if (!ISipmilanDEV(s)) {
+		syslog(LOG_ERR, "invalid stonith device argument to %s", __FUNCTION__);
+		return(S_OOPS);
+	}
+
+	nd = (struct ipmilanDevice *)s->pinfo;
+	node = nd->hostlist;
+	do {
+		if (strcmp(node->hostname, host) == 0) {
+			break;
+		};
+
+		node = node->next;
+	} while (node);
+
+	if (!node) {
+		syslog(LOG_ERR, _("host %s is not configured in this STONITH module. Please check you configuration file."), host);
+		return (S_OOPS);
+	}
+
+	rc = do_ipmi_cmd(node, request);
+	if (!rc) {
+		syslog(LOG_INFO, _("Host %s ipmilan-reset."), host);
+	} else {
+		syslog(LOG_INFO, _("Host %s ipmilan-reset error. Error = %d."), host, rc);
+	}
+	return rc;
+}
+
+/*
+ *	Parse the information in the given configuration file,
+ *	and stash it away...
+ */
+static int
+ipmilan_set_config_file(Stonith* s, const char * configname)
+{
+	FILE *	cfgfile;
+
+	char	ipmiline[256];
+
+	struct ipmilanDevice*	nd;
+
+	int rc = S_BADCONFIG;
+
+	if (!ISipmilanDEV(s)) {
+		syslog(LOG_ERR, "invalid argument to ipmilan_set_configfile");
+		return(S_OOPS);
+	}
+	nd = (struct ipmilanDevice*) s->pinfo;
+
+	if ((cfgfile = fopen(configname, "r")) == NULL)  {
+		syslog(LOG_ERR, "Cannot open %s", configname);
+		return(S_BADCONFIG);
+	}
+	while (fgets(ipmiline, sizeof(ipmiline), cfgfile) != NULL){
+		if (*ipmiline == '#' || *ipmiline == '\n' || *ipmiline == EOS) {
+			continue;
+		}
+		if ((rc = ipmilan_parse_config_info(nd, ipmiline)) != S_OK) {
+			break;
+		};
+	}
+
+	fclose(cfgfile);
+	return(rc);
+}
+
+/*
+ *	Parse the config information in the given string, and stash it away...
+ */
+static int
+ipmilan_set_config_info(Stonith* s, const char * info)
+{
+	struct ipmilanDevice* nd;
+
+	if (!ISipmilanDEV(s)) {
+		syslog(LOG_ERR, "%s: invalid argument", __FUNCTION__);
+		return(S_OOPS);
+	}
+	nd = (struct ipmilanDevice *)s->pinfo;
+
+	return(ipmilan_parse_config_info(nd, info));
+}
+
+static const char *
+ipmilan_getinfo(Stonith * s, int reqtype)
+{
+	struct ipmilanDevice* nd;
+	char *		ret;
+
+	if (!ISipmilanDEV(s)) {
+		syslog(LOG_ERR, "ipmilan_getinfo: invalid argument");
+		return NULL;
+	}
+	/*
+	 *	We look in the ST_TEXTDOMAIN catalog for our messages
+	 */
+	nd = (struct ipmilanDevice *)s->pinfo;
+
+	switch (reqtype) {
+		case ST_DEVICEID:
+			ret = _("ipmilan STONITH device");
+			break;
+
+		case ST_CONF_INFO_SYNTAX:
+			ret = _("hostname ipaddr port auth priv user pass \n"
+			"all fields are white-space delimited.");
+			break;
+
+		case ST_CONF_FILE_SYNTAX:
+			ret = _("hostname ipaddr port auth priv user pass \n"
+			"All fields are white-space delimited.  "
+			"Blank lines and lines beginning with # are ignored");
+			break;
+
+		case ST_DEVICEDESCR:
+			ret = _("IPMI_LAN STONITH device\n");
+			break;
+
+		default:
+			ret = NULL;
+			break;
+	}
+	return ret;
+}
+
+/*
+ *	ipmilan Stonith destructor...
+ *
+ * 	The hostlist is a link list.  So have to iterate through.
+ */
+static void
+ipmilan_destroy(Stonith *s)
+{
+	struct ipmilanDevice* nd;
+	struct ipmilanHostInfo * host;
+	int i;
+
+	if (!ISipmilanDEV(s)) {
+		syslog(LOG_ERR, "%s: invalid argument", __FUNCTION__);
+		return;
+	}
+	nd = (struct ipmilanDevice *)s->pinfo;
+
+	nd->ipmiid = NOTipmilanID;
+
+	host = nd->hostlist->prev;
+	for (i = 0; i < nd->hostcount; i++) {
+
+		FREE(host->hostname);
+		FREE(host->ipaddr);
+
+		FREE(host);
+		host = host->prev;
+	}
+
+	nd->hostcount = -1;
+	FREE(nd);
+}
+
+/* Create a new ipmilan Stonith device.  Too bad this function can't be static */
+static void *
+ipmilan_new(void)
+{
+	struct ipmilanDevice*	nd = MALLOCT(struct ipmilanDevice);
+
+	if (nd == NULL) {
+		syslog(LOG_ERR, "out of memory");
+		return(NULL);
+	}
+	memset(nd, 0, sizeof(*nd));
+	nd->ipmiid = ipmilanid;
+	nd->hostlist = NULL;
+	nd->hostcount = 0; 
+	return((void *)nd);
+}
+
+
---
+++ lib/plugins/stonith/ipmilan_command.c
@@ -0,0 +1,340 @@
+/*
+ * This program is largely based on the ipmicmd.c program that's part of OpenIPMI package.
+ * 
+ * Copyright Intel Corp. 
+ * Yixiong.Zou@intel.com
+ */
+
+#include <stdio.h>
+
+#include <stdlib.h> // malloc()
+#include <unistd.h> // getopt()
+#include <string.h> // strerror()
+#include <netdb.h> // gethostbyname()
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <syslog.h>
+
+#include <OpenIPMI/selector.h>
+#include <OpenIPMI/ipmi_conn.h>
+#include <OpenIPMI/ipmi_lan.h>
+#include <OpenIPMI/ipmi_smi.h>
+#include <OpenIPMI/ipmi_auth.h>
+#include <OpenIPMI/ipmi_msgbits.h>
+
+#include "ipmilan.h"
+#include <stonith/stonith.h>
+
+// #define DUMP_MSG 0
+#define OPERATION_TIME_OUT 10
+
+selector_t *os_sel;
+extern os_handler_t ipmi_os_cb_handlers;
+
+typedef enum ipmi_status {
+	/*
+	IPMI_CONNECTION_FAILURE,
+	IPMI_SEND_FAILURE,
+	IPMI_BAD_REQUEST,
+	IPMI_REQUEST_FAILED,
+	IPMI_TIME_OUT,
+	*/
+	IPMI_RUNNING = 99,
+} ipmi_status_t;
+
+static ipmi_status_t gstatus;
+
+typedef enum chassis_control_request {
+	POWER_DOWN = 0X00,
+	POWER_UP = 0X01,
+	POWER_CYCLE = 0X02,
+	HARD_RESET = 0X03,
+	PULSE_DIAGNOSTIC_INTERRUPT = 0X04,
+	SOFT_SHUTDOWN = 0X05
+} chassis_control_request_t;
+
+void dump_msg_data(ipmi_msg_t *msg, ipmi_addr_t *addr, char *type);
+void rsp_handler(ipmi_con_t *ipmi,
+		ipmi_addr_t *addr,
+		unsigned int addr_len,
+		ipmi_msg_t *rsp,
+		void *data1,
+		void *data2,
+		void *data3,
+		void *data4);
+
+void send_ipmi_cmd(ipmi_con_t *con, int request);
+
+void timed_out(selector_t *sel, sel_timer_t *timer, void *data);
+
+void 
+timed_out(selector_t  *sel, sel_timer_t *timer, void *data)
+{
+	syslog(LOG_ERR, "IPMI operation timed out... :(\n");
+	gstatus = S_TIMEOUT;
+}
+
+void
+dump_msg_data(ipmi_msg_t *msg, ipmi_addr_t *addr, char *type)
+{
+	ipmi_system_interface_addr_t *smi_addr = NULL;
+	int i;
+	ipmi_ipmb_addr_t *ipmb_addr = NULL;
+
+	if (addr->addr_type == IPMI_SYSTEM_INTERFACE_ADDR_TYPE) {
+		smi_addr = (struct ipmi_system_interface_addr *) addr;
+
+	} else if ((addr->addr_type == IPMI_IPMB_ADDR_TYPE) 
+			|| (addr->addr_type == IPMI_IPMB_BROADCAST_ADDR_TYPE)) {
+		ipmb_addr = (struct ipmi_ipmb_addr *) addr;
+	}
+
+	if (smi_addr) {
+		fprintf(stderr, "%2.2x %2.2x %2.2x %2.2x ", 
+			addr->channel,
+			msg->netfn,
+			smi_addr->lun,
+			msg->cmd);
+	} else {
+		fprintf(stderr, "%2.2x %2.2x %2.2x %2.2x ", 
+			addr->channel,
+			msg->netfn,
+			ipmb_addr->lun,
+			msg->cmd);
+	}
+
+	for (i = 0; i < msg->data_len; i++) {
+		if (((i%16) == 0) && (i != 0)) {
+			printf("\n            ");
+		}
+		fprintf(stderr, "%2.2x ", msg->data[i]);
+	}
+	fprintf(stderr, "\n");
+}
+
+/*
+ * This function gets called after the response comes back
+ * from the IPMI device. 
+ * 
+ * Some IPMI device does not return success, 0x00, to the 
+ * remote node when the power-reset was issued.
+ * 
+ * The host who sent the ipmi cmd might get a 0xc3,
+ * a timeout instead.  This creates problems for 
+ * STONITH operation, where status is critical. :( 
+ * 
+ * Right now I am only checking 0xc3 as the return.
+ * If your IPMI device returns some wired code after 
+ * reset, you might want to add it in this code block.
+ *
+ */
+
+void
+rsp_handler(ipmi_con_t *ipmi,
+		ipmi_addr_t *addr,
+		unsigned int addr_len,
+		ipmi_msg_t *rsp,
+		void *data1,
+		void *data2,
+		void *data3,
+		void *data4)
+{
+	int rv;
+	int * request;
+
+	request = (int *) data1;
+
+#if 0
+	dump_msg_data(rsp, addr, NULL);
+#endif
+	rv = rsp->data[0];  
+	/* some IPMI device might not issue 0x00, success, for reset command.
+	   instead, a 0xc3, timeout, is returned. */
+	if (rv == 0x00 || 
+		(rv == 0xc3 && *request <= ST_POWEROFF && *request >= ST_GENERIC_RESET ) ) {
+		gstatus = S_OK;
+	} else {
+		gstatus = S_RESETFAIL;
+	}
+
+	free(request);
+	return;
+}
+
+void
+send_ipmi_cmd(ipmi_con_t *con, int request)
+{
+	ipmi_addr_t addr;
+	unsigned int addr_len;
+	ipmi_msg_t msg;
+	struct ipmi_system_interface_addr *si;
+	int rv;
+	int * data1;
+	// chassis control command request is only 1 byte long
+	unsigned char cc_data = POWER_CYCLE; 
+
+	si = (void *) &addr;
+	si->lun = 0x00;
+	si->channel = IPMI_BMC_CHANNEL;
+	si->addr_type = IPMI_SYSTEM_INTERFACE_ADDR_TYPE;
+	addr_len = sizeof(*si);
+
+	msg.netfn = IPMI_CHASSIS_NETFN;
+	msg.cmd = IPMI_CHASSIS_CONTROL_CMD;
+	msg.data = &cc_data;
+	msg.data_len = 1;
+
+	switch (request) {
+		case ST_POWERON:
+			cc_data = POWER_DOWN;
+			break;
+
+		case ST_POWEROFF:
+			cc_data = POWER_UP;
+			break;
+
+		case ST_GENERIC_RESET:
+			cc_data = POWER_CYCLE;
+			break;
+
+		case ST_IPMI_STATUS:
+			msg.netfn = IPMI_APP_NETFN;
+			msg.cmd = IPMI_GET_DEVICE_ID_CMD;
+			msg.data = NULL;
+			msg.data_len = 0;
+			break;
+
+		default:
+			gstatus = S_INVAL;
+			return;
+	}
+
+	data1 = (int *) malloc(sizeof(int));
+	*data1 = request;
+
+	rv = con->send_command(con, &addr, addr_len, &msg, rsp_handler, 
+				data1, NULL, NULL, NULL);
+	if (rv == -1) {
+		syslog(LOG_ERR, "Error sending IPMI command: %x\n", rv);
+		gstatus = S_ACCESS;
+	}
+
+	return;
+}
+
+static void
+con_changed_handler(ipmi_con_t *ipmi,
+			int err,
+			unsigned int port_num,
+			int still_connected,
+			void *cb_data)
+{
+	int * request;
+	if (err) {
+		syslog(LOG_ERR, "Unable to setup connection: %x\n", err);
+		return;
+	}
+
+	request = (int *) cb_data;
+	send_ipmi_cmd(ipmi, *request);
+}
+
+
+static int
+setup_ipmi_conn(struct ipmilanHostInfo * host, int request)
+{
+	int rv;
+
+	struct hostent *ent;
+	struct in_addr lan_addr[2];
+	int lan_port[2];
+	int num_addr = 1;
+	int authtype = 0;
+	int privilege = 0;
+	char username[17];
+	char password[17];
+
+	static ipmi_con_t *con;
+
+	sel_timer_t * timer;
+	struct timeval timeout;
+
+	rv = ipmi_init(&ipmi_os_cb_handlers);
+	if (rv) {
+		syslog(LOG_ERR, "ipmi_init erro: %d ", rv);
+		return rv;
+	}
+
+	rv = sel_alloc_selector(&os_sel);
+	if (rv) {
+		syslog(LOG_ERR, "Could not alloctate selector\n");
+		return rv;
+	}
+
+	ent = gethostbyname(host->ipaddr);
+	if (!ent) {
+		syslog(LOG_ERR, "gethostbyname failed: %s\n", strerror(h_errno));
+		return 1;
+	}
+
+	memcpy(&lan_addr[0], ent->h_addr_list[0], ent->h_length);
+	lan_port[0] = host->portnumber;
+	lan_port[1] = 0;
+
+	authtype = host->authtype;
+	privilege = host->privilege;
+
+	memcpy(username, host->username, sizeof(username));
+	memcpy(password, host->password, sizeof(password));
+
+	rv = ipmi_lan_setup_con(lan_addr, lan_port, num_addr, 
+				authtype, privilege,
+				username, strlen(username),
+				password, strlen(password),
+				&ipmi_os_cb_handlers, os_sel,
+				&con);
+
+	if (rv) {
+		syslog(LOG_ERR, "ipmi_lan_setup_con: %s\n", strerror(rv));
+		return S_ACCESS;
+	}
+
+	con->set_con_change_handler(con, con_changed_handler, &request);
+
+	gstatus = IPMI_RUNNING;
+
+	rv = con->start_con(con);
+	if (rv) {
+		syslog(LOG_ERR, "Could not start IPMI connection: %x\n", rv);
+		gstatus = S_BADCONFIG;
+		return rv;
+	}
+
+	gettimeofday(&timeout, NULL);
+	timeout.tv_sec += OPERATION_TIME_OUT;
+	timeout.tv_usec += 0;
+
+	sel_alloc_timer(os_sel, timed_out, NULL, &timer);
+	sel_start_timer(timer, &timeout);
+
+        while (1) {
+                rv = sel_select(os_sel, NULL, 0, NULL, NULL);
+		if (gstatus != IPMI_RUNNING) {
+			break;
+		}
+        }
+
+	sel_free_timer(timer);
+
+	con->close_connection(con);
+	ipmi_shutdown();
+	return gstatus;
+}
+
+int
+do_ipmi_cmd(struct ipmilanHostInfo * host, int request)
+{
+	return setup_ipmi_conn(host, request);
+}
+
+
