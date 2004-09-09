7aad95df16de366a9619fab2a4fc40c6cdcca6e1
zhenh <zhenh@localhost>
2004-09-09 03:25:15 +0000

    remove unuse struct
    CVS patchset: 3664
    CVS date: 2004/09/09 03:25:15

--- include/lrm/racommon.h
+++ include/lrm/racommon.h
@@ -5,15 +5,8 @@ extern const int RA_MAX_NAME_LENGTH;
 extern const int RA_MAX_DIRNAME_LENGTH;
 extern const int RA_MAX_BASENAME_LENGTH;
 
-typedef struct {
-	gchar * rsc_type;
-	/* As for version, no definite definition yet */
-	gchar * version;
-} rsc_info_t;
-
 void get_ra_pathname(const char* class_path, const char* type, const char* provider, char pathname[]);
 gboolean filtered(char * file_name);
-int get_providers(const char* class_path, const char* op_type, GList ** providers);
-int get_ra_list(const char* class_path, GList ** rsc_info);
+int get_runnable_list(const char* class_path, GList ** rsc_info);
 
 #endif /* RACOMMON_H */
