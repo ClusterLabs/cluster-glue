76029f9f3f52727b94d8923eba4ee2d863e176ce
alan <unknown>
2005-01-05 04:04:01 +0000

    Forgot to commit this change :-(. Such sloppiness!!
    CVS patchset: 4549
    CVS date: 2005/01/05 04:04:01

--- include/clplumbing/ttylock.h
+++ include/clplumbing/ttylock.h
@@ -1,8 +1,6 @@
-/* $Id: ttylock.h,v 1.1 2004/03/25 08:20:34 alan Exp $ */
+/* $Id: ttylock.h,v 1.2 2005/01/05 04:04:01 alan Exp $ */
 #ifndef __CLPLUMBING_TTYLOCK_H
 #	define __CLPLUMBING_LOCK_H
 int	ttylock(const char *serial_device);
 int	ttyunlock(const char *serial_device);
-int	DoLock(const char * prefix, const char *lockname);
-int	DoUnlock(const char * prefix, const char *lockname);
 #endif	/* __CLPLUMBING_LOCK_H */
