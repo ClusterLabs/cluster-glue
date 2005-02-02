bf1bd4ccff46b989c55315692520815c92ec862e
gshi <unknown>
2005-02-02 08:13:50 +0000

    fixed a bug about wrong entity in cl_log
    CVS patchset: 4693
    CVS date: 2005/02/02 08:13:50

--- include/clplumbing/loggingdaemon.h
+++ include/clplumbing/loggingdaemon.h
@@ -1,12 +1,14 @@
-/* $Id: loggingdaemon.h,v 1.4 2004/11/18 02:27:18 gshi Exp $ */
+/* $Id: loggingdaemon.h,v 1.5 2005/02/02 08:13:50 gshi Exp $ */
 /* Messages sent to the logging daemon */
 #define	LD_LOGIT	2
+#define MAXENTITY	32
 struct LogDaemonMsg_s {
 	int		msgtype;
 	int		facility;
 	int		priority;
 	int		msglen;
 	gboolean	use_pri_str;
+	char		entity[MAXENTITY];
 	char		message[0]; /* Actually much bigger ;-) */
 };
 typedef	struct LogDaemonMsg_s	LogDaemonMsg;
