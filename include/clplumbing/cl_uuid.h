7c5cf84e6bd0790fe67fd7938df45c28eeba0756
gshi <unknown>
2005-04-27 05:31:42 +0000

    use struct cl_uuid_t to replace uuid_t use cl_uuid_xxx to replace uuid_xxx funcitons
    CVS patchset: 5973
    CVS date: 2005/04/27 05:31:42

---
+++ include/clplumbing/cl_uuid.h
@@ -0,0 +1,22 @@
+#ifndef _CL_UUID_H_
+#define _CL_UUID_H_
+
+#include <uuid/uuid.h>
+
+typedef struct cl_uuid_s{	
+	uuid_t uuid;	
+}cl_uuid_t;
+
+void cl_uuid_copy(cl_uuid_t* dst, cl_uuid_t* src);
+void cl_uuid_clear(cl_uuid_t* uu);
+int cl_uuid_compare(cl_uuid_t* uu1, cl_uuid_t* uu2);
+void cl_uuid_generate(cl_uuid_t* out);
+int cl_uuid_is_null(cl_uuid_t* uu);
+time_t cl_uuid_time(cl_uuid_t* uu, struct timeval *ret_tv);
+int cl_uuid_parse( char *in, cl_uuid_t* uu);
+void cl_uuid_unparse(cl_uuid_t* uu, char *out);
+
+
+#endif
+
+
