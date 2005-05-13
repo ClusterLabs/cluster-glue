c0c9c3490000f9bbf69061c23c1299ebbf399d33
gshi <gshi@localhost>
2005-05-13 23:07:55 +0000

    add one function cl_read_pidfile_no_checking()
    CVS patchset: 6272
    CVS date: 2005/05/13 23:07:55

--- include/clplumbing/cl_pidfile.h
+++ include/clplumbing/cl_pidfile.h
@@ -2,6 +2,7 @@
 #define _LOCKFILE_H_
 
 int	cl_read_pidfile(const char *filename);
+int	cl_read_pidfile_no_checking(const char *filename);
 int	cl_lock_pidfile(const char *filename);
 int	cl_unlock_pidfile(const char *filename);
 
