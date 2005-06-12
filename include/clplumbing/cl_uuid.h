407fbd001c3ff7a46351d93275d67f05ceeabc38
Alan Robertson <alanr@unix.sh>
2005-06-12 06:11:18 +0000

    The uuid code didn't make an RPM correctly, and when I got to looking into it, I decided to make some minor rearrangements while I was fixing the RPM problem.
    CVS patchset: 6621
    CVS date: 2005/06/12 06:11:18

--- include/clplumbing/cl_uuid.h
+++ include/clplumbing/cl_uuid.h
@@ -4,7 +4,7 @@
 #include <hb_uuid.h>
 
 typedef struct cl_uuid_s{	
-	uuid_t uuid;	
+	unsigned char	uuid[8];
 }cl_uuid_t;
 
 void cl_uuid_copy(cl_uuid_t* dst, cl_uuid_t* src);
@@ -12,7 +12,6 @@ void cl_uuid_clear(cl_uuid_t* uu);
 int cl_uuid_compare(cl_uuid_t* uu1, cl_uuid_t* uu2);
 void cl_uuid_generate(cl_uuid_t* out);
 int cl_uuid_is_null(cl_uuid_t* uu);
-time_t cl_uuid_time(cl_uuid_t* uu, struct timeval *ret_tv);
 int cl_uuid_parse( char *in, cl_uuid_t* uu);
 void cl_uuid_unparse(cl_uuid_t* uu, char *out);
 
