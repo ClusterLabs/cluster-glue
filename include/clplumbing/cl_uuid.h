a7bfa6a873188402e5f7bd2940c40c3dacc50d08
alan <unknown>
2005-06-12 06:40:34 +0000

    Fixing up last changes :-(
    CVS patchset: 6623
    CVS date: 2005/06/12 06:40:34

--- include/clplumbing/cl_uuid.h
+++ include/clplumbing/cl_uuid.h
@@ -7,7 +7,7 @@ typedef struct cl_uuid_s{
 
 void cl_uuid_copy(cl_uuid_t* dst, cl_uuid_t* src);
 void cl_uuid_clear(cl_uuid_t* uu);
-int cl_uuid_compare(cl_uuid_t* uu1, cl_uuid_t* uu2);
+int cl_uuid_compare(const cl_uuid_t* uu1, const cl_uuid_t* uu2);
 void cl_uuid_generate(cl_uuid_t* out);
 int cl_uuid_is_null(cl_uuid_t* uu);
 int cl_uuid_parse( char *in, cl_uuid_t* uu);
