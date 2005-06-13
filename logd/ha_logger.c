90bd04cd78d658262fe570d67146c776ac051a7b
davidlee <davidlee@localhost>
2005-06-13 14:16:52 +0000

    ensure '#include <portability.h>' is present and correctly positioned
    CVS patchset: 6650
    CVS date: 2005/06/13 14:16:52

--- logd/ha_logger.c
+++ logd/ha_logger.c
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
+#include <portability.h>
 #include <glib.h>
 #include <clplumbing/cl_log.h>
 #include <clplumbing/ipc.h>
