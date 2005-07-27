82b70d91439dede1deded2a7c5f176abe836b3af
panjiam <unknown>
2005-07-27 01:50:51 +0000

    added license statements
    CVS patchset: 7105
    CVS date: 2005/07/27 01:50:51

--- lib/plugins/stonith/ipmi_os_handler.c
+++ lib/plugins/stonith/ipmi_os_handler.c
@@ -1,11 +1,24 @@
-/* $Id: ipmi_os_handler.c,v 1.3 2004/08/17 21:58:46 yixiong Exp $ */
+/* $Id: ipmi_os_handler.c,v 1.4 2005/07/27 01:50:51 panjiam Exp $ */
 /*
  * This program is largely based on the ipmicmd.c program that's part of OpenIPMI package.
  * 
  * Copyright Intel Corp. 
  * Yixiong.Zou@intel.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 
 #include <stdlib.h>
 #include <errno.h>
