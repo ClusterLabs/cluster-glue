134e58293a8781dcd65cdea4b1e2031ad6e63abd
sunjd <unknown>
2005-07-28 08:32:03 +0000

    bug668: license update
    CVS patchset: 7125
    CVS date: 2005/07/28 08:32:03

--- include/lrm/racommon.h
+++ include/lrm/racommon.h
@@ -1,3 +1,22 @@
+/*
+ * Author: Sun Jiang Dong <sunjd@cn.ibm.com>
+ * Copyright (c) 2004 International Business Machines
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #ifndef RACOMMON_H
 #define RACOMMON_H
 
