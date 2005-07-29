c9df34fec8c468d47bb5b539864ac1e49d5fbbc8
panjiam <panjiam@localhost>
2005-07-29 05:31:27 +0000

    added license statements, bug # 668
    CVS patchset: 7136
    CVS date: 2005/07/29 05:31:27

--- lib/clplumbing/mkstemp_mode.c
+++ lib/clplumbing/mkstemp_mode.c
@@ -1,4 +1,20 @@
-/* $Id: mkstemp_mode.c,v 1.5 2004/02/17 22:11:59 lars Exp $ */
+/* $Id: mkstemp_mode.c,v 1.6 2005/07/29 05:31:27 panjiam Exp $ */
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include <portability.h>
 #include <stdlib.h>
 #include <unistd.h>
--- lib/clplumbing/timers.c
+++ lib/clplumbing/timers.c
@@ -1,4 +1,20 @@
-/* $Id: timers.c,v 1.10 2005/06/19 02:59:30 alan Exp $ */
+/* $Id: timers.c,v 1.11 2005/07/29 05:31:27 panjiam Exp $ */
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include <portability.h>
 #include <sys/time.h>
 #include <unistd.h>
