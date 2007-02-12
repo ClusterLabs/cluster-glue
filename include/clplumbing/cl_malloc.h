a9e4868a37de6060daaca0fd6744edba2b93524c
Andrew Beekhof <beekhof@gmail.com>
2007-02-12 11:13:13 +0100

    Build: Make supplying a Valgrind suppression file simpler Also decouple using Valgrind from enabling libc malloc

--- include/clplumbing/cl_malloc.h
+++ include/clplumbing/cl_malloc.h
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-#ifdef WITH_VALGRIND
+#ifdef USE_LIBC_MALLOC
 /* Use libc malloc and friends.  Useful when running valgrind etc. */
 #  ifndef _CLPLUMBING_CLMALLOC_NATIVE_H
 #  define _CLPLUMBING_CLMALLOC_NATIVE_H
