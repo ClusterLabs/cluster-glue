bb8145df696af49c0181e1cedb7b3360ab93dd68
Dejan Muhamedagic <dejanmm@fastmail.fm>
2008-05-26 14:11:51 +0200

    Low: logging (lrmd/stonithd): reduce logging on repeating monitor ops

--- lrm/lrmd/lrmd.c
+++ lrm/lrmd/lrmd.c
@@ -2975,7 +2975,7 @@ perform_ra_op(lrmd_op_t* op)
 		default:	/* Parent */
 			child_count++;
 			NewTrackedProc(pid, 1
-			,	debug_level ? PT_LOGVERBOSE : PT_LOGNONE
+			,	debug_level ? (op->interval ? PT_LOGNORMAL : PT_LOGVERBOSE) : PT_LOGNONE
 			,	op, &ManagedChildTrackOps);
 
 			close(stdout_fd[1]);
