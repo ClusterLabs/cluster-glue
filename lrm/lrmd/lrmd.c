f6e1784b6335750cdbc81b226ba85012e27f4d0a
Dejan Muhamedagic <dejanmm@fastmail.fm>
2008-08-04 18:02:20 +0200

    Low: lrmd: reduce the severity on async notification when the client has already signed out

--- lrm/lrmd/lrmd.c
+++ lrm/lrmd/lrmd.c
@@ -2051,8 +2051,8 @@ async_notify(gpointer key, gpointer val, gpointer data)
 
 	client = lookup_client_by_name((char *)key);
 	if (!client) {
-		lrmd_log(LOG_ERR,
-			"%s: strange, client %s not found", __FUNCTION__, (char *)key);
+		lrmd_log(LOG_INFO,
+			"%s: client %s not found, probably signed out", __FUNCTION__, (char *)key);
 		return;
 	}
 	if (HA_OK != ha_msg_mod(msg,F_LRM_APP,client->app_name)) {
